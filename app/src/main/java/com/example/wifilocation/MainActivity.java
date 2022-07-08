18ee104f61b10256570e9a249d14f5af99412c2a
Gunp <1610924363@qq.com>
2022-07-08 13:49:57 +0800

    add Array

--- app/src/main/java/com/example/wifilocation/MainActivity.java
+++ app/src/main/java/com/example/wifilocation/MainActivity.java
@@ -20,13 +20,16 @@ public class MainActivity extends Activity {
     private final String TAG = "Gunp";
     private static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1000;
     private final int UPDATE_UI_REQUEST_CODE = 1024;
-    private final String BASE1 = "lirex758002";
-    private final String BASE2 = "HUAWEI-SENSOR";
-    private final String BASE3 = "CMCC-FCfP";
+    private final String BASE0 = "lirex758002";
+    private final String BASE1 = "HUAWEI-SENSOR";
+    private final String BASE2 = "CMCC-FCfP";
+    private final String BASE3 = "TP-LINK_A494";
+    private final String BASE4 = "203";
 
     private TextView mScanResultTV;    // 显示WiFi扫描结果的控件
     private Button scanButton;
     private StringBuffer mScanResultStr;    // 暂存WiFi扫描结果的字符串
+    private Integer[] result;
     private WifiManager mWifiManager;   // 调用WiFi各种API的对象
     private Handler mHandler = new Handler() {
         @Override
@@ -83,18 +86,34 @@ public class MainActivity extends Activity {
         // 获取并保存WiFi扫描结果
         Log.d(TAG, "scanWifi: ");
         mScanResultStr = new StringBuffer();
+        result = new Integer[]{-150, -150, -150, -150, -150};
         List<ScanResult> scanResults = mWifiManager.getScanResults();
         for (ScanResult sr : scanResults) {
-            if (!sr.SSID.equals(BASE1) && !sr.SSID.equals(BASE2) && !sr.SSID.equals((BASE3))) continue;
-            Log.d(TAG, "scanWifi2: ");
+            switch (sr.SSID) {
+                case BASE0:
+                    result[0] = sr.level;
+                    break;
+                case BASE1:
+                    result[1] = sr.level;
+                    break;
+                case BASE2:
+                    result[2] = sr.level;
+                    break;
+                case BASE3:
+                    result[3] = sr.level;
+                    break;
+                case BASE4:
+                    result[4] = sr.level;
+                    break;
+            }
             mScanResultStr.append("SSID: ").append(sr.SSID).append("\n");
             mScanResultStr.append("MAC Address: ").append(sr.BSSID).append("\n");
             mScanResultStr.append("Signal Strength(dBm): ").append(sr.level).append("\n\n");
-
             Log.d(TAG, "SSID:" + sr.SSID + "  MAC Address: " + sr.BSSID + "  Signal Strength:" + sr.level);
 
             // TODO 将结果存入数据库
         }
+        Log.d(TAG, "RESULT: " + result[0] + "  " + result[1] + "  " + result[2] + "  " + result[3] + "  " + result [4]);
     }
 
     private void updateUI() {
